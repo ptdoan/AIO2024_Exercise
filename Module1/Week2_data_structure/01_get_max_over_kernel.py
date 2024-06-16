1441f330fc331db190217fda6c48c2adfba821bf
dpthinh <doanphucthinh@gmail.com>
2024-06-16 15:54:52 +0700

    combine commit

--- Module1/Week2_data_structure/01_get_max_over_kernel.py
+++ Module1/Week2_data_structure/01_get_max_over_kernel.py
@@ -1,8 +1,13 @@
 # Exercise1_get_max_over_kernel
-k = 3
-num_list = [3,4,5,1,-44,5,10,12,33,1]
-result = []
-for i in range(len(num_list) + 1 - k):
-    result.append(max(num_list[i:i+k]))
+def max_kernel(num_list, k):
+    result = []
+    for i in range(len(num_list) + 1 - k):
+        result.append(max(num_list[i:i+k]))
+    return result
 
-print(result)
+
+assert max_kernel([3, 4, 5, 1, -44], 3) == [5, 5, 5]
+num_list = [3, 4, 5, 1, -44, 5, 10, 12, 33, 1]
+k = 3
+print(max_kernel(num_list, k))
+print()
