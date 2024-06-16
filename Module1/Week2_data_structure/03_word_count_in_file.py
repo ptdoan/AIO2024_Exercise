46f21c38848abefc2404fb2753ec67951b434e25
dpthinh <doanphucthinh@gmail.com>
2024-06-16 16:08:12 +0700

    Sonar check

--- Module1/Week2_data_structure/03_word_count_in_file.py
+++ Module1/Week2_data_structure/03_word_count_in_file.py
@@ -1,33 +1,31 @@
 # Exercise3_count_word_in_file
-def preprocess_text(sentence):
-  sentence = sentence.lower()
-  sentence = sentence.strip()
-  sentence = sentence.replace(',', '')
-  sentence = sentence.replace('.', '')
-  word = sentence.split()
-  return word
-
-sentence = 'I love AI. AI    is not easy.'
-preprocess_text(sentence)
-
-
 
 # !gdown https://drive.google.com/uc?id=1IBScGdW2xlNsc9v5zSAya548kNgiOrko
+# path = 'P1_data.txt'
 
-path = 'P1_data.txt'
-with open(path, 'r') as f:
-  sentences = f.readlines()
-type(sentences)
+def preprocess_text(sentence):
+    sentence = sentence.lower()
+    sentence = sentence.strip()
+    sentence = sentence.replace(',', '')
+    sentence = sentence.replace('.', '')
+    word = sentence.split()
+    return word
 
-sentences[:len(sentences)]
 
-counter = {}
-for sentence in sentences:
-  words = preprocess_text(sentence)
-  for word in words:
-    if word in counter:
-      counter[word] += 1
-    else:
-      counter[word] = 1
+def count_word(file_path):
+    counter = {}
+    with open(file_path, 'r') as f:
+        sentences = f.readlines()
+    for sentence in sentences:
+        words = preprocess_text(sentence)
+        for word in words:
+            if word in counter:
+                counter[word] += 1
+            else:
+                counter[word] = 1
+    return counter
 
-print(counter)
+
+result = count_word('/Module1/Week2_data_structure/P1_data.txt')
+assert result['who'] == 3
+print(result['man'])
